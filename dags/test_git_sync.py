2e8ee9bfbe117b5f4195cc7b6d13f7e871ce890d
choiyounghwan123 <fdgdfgdgf123@naver.com>
2025-08-18 07:29:36 +0000

    test

---
+++ dags/test_git_sync.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Git Sync 동작 확인!")
+    return "ok"
+
+dag = DAG(
+    dag_id="test_git_sync",
+    start_date=datetime(2025, 8, 18),
+    schedule_interval=None,
+    catchup=False,
+)
+
+PythonOperator(
+    task_id="print_hello",
+    python_callable=hello,
+    dag=dag
+)
